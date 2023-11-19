ab7d29e9daf3ceec1b92d73ff8274a6bbaaf733b
nestorrig <neriga10@hotmail.com>
2023-11-18 23:29:38 -0600

    Proyect: add button Task styles

--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -1,9 +1,14 @@
+import "./styles/CreateTodoButton.css"
 function CreateTodoButton() {
     return (
-        <button>
-            +
-        </button>
-    )
+        <>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
+                <path fill="#ffffff" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"></path>
+                </svg>
+            </button>
+        </>
+    );
 }
 
 export { CreateTodoButton }
