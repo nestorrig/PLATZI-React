332060aae68ec81b07133b9b949b9262f4de3a33
nestorrig <neriga10@hotmail.com>
2023-11-25 18:10:38 -0600

    Clase 7: ¿Qué es el estado?

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react";
 import "./styles/TodoSearch.css"
+
 function TodoSearch() {
+    const [searchValue, setSearchValue] = useState("");
+    useEffect(() => {
+        if (searchValue === "") {
+            return;
+        }
+        console.log('los valores de searchValue son: ' + searchValue);
+    }, [searchValue]);
+
     return (
         <div className="TodoSearch">
             <input 
                 placeholder="Cortar cebolla" 
                 type="text"
+                value={searchValue}
                 onChange={(event) => {
-                    console.log(event.target.value);
+                    setSearchValue(event.target.value);
                 }}
             />
         </div>
