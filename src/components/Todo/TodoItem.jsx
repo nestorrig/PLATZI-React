836f98b8029cd3ce698c7441d53bba7913bc1368
Nestor Rios Garcia <neriga10@hotmail.com>
2024-03-16 19:18:19 -0600

    feat: fade delete task

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -17,38 +17,44 @@ function TodoItem({ text, completed, onCheck, onDelete }) {
   }, [isDeleting, onDelete]);
 
   return (
-    <div className={`grid grid-rows-1 ${isDeleting ? "disappear" : ""}`}>
-      <li
-        className={`relative p-4 flex items-center gap-4 bg-light-bg-200 dark:bg-dark-bg-200 rounded-full rounded-tr-none ${
-          isDeleting ? "" : ""
-        }`}
-      >
-        <button
-          className={`w-6 h-6 border-2 rounded-full cursor-pointer ease-in-out duration-300 active:scale-75 ${
-            completed
-              ? "border-light-bg-300 dark:border-dark-bg-300 bg-light-bg-300 dark:bg-dark-bg-300"
-              : "border-light-bg-300 dark:border-dark-bg-300"
-          }`}
-          onClick={onCheck}
-        >
-          <CompletedIcon completed={completed} />
-        </button>
-        <p
-          className={`transition-all duration-300 ${
-            completed
-              ? "line-through text-light-text-200 dark:text-dark-text-200"
-              : ""
+    <div
+      className={`grid origin-top transition-all duration-300 ease-in-out ${
+        isDeleting ? "grid-rows-[0fr] mb-0" : "grid-rows-[1fr] mb-4"
+      }`}
+    >
+      <div className={` ${isDeleting ? "overflow-hidden" : ""}`}>
+        <li
+          className={`relative p-4 flex items-center gap-4 transition-all origin-top duration-300 bg-light-bg-200 dark:bg-dark-bg-200 rounded-full rounded-tr-none ${
+            isDeleting ? "scale-y-0 " : ""
           }`}
         >
-          {text}
-        </p>
-        <button
-          className="absolute right-[-14px] top-[-14px] w-[30px] h-[30px] flex justify-center items-center bg-light-bg-300 dark:bg-dark-bg-300 border-none rounded-full p-1 cursor-pointer transition-background ease-in-out duration-300 hover:bg-light-primary-100 hover:dark:bg-dark-primary-100"
-          onClick={() => setIsDeleting(true)}
-        >
-          <DeleteIcon />
-        </button>
-      </li>
+          <button
+            className={`w-6 h-6 border-2 rounded-full cursor-pointer ease-in-out duration-300 active:scale-75 ${
+              completed
+                ? "border-light-bg-300 dark:border-dark-bg-300 bg-light-bg-300 dark:bg-dark-bg-300"
+                : "border-light-bg-300 dark:border-dark-bg-300"
+            }`}
+            onClick={onCheck}
+          >
+            <CompletedIcon completed={completed} />
+          </button>
+          <p
+            className={`transition-all duration-300 ${
+              completed
+                ? "line-through text-light-text-200 dark:text-dark-text-200"
+                : ""
+            }`}
+          >
+            {text}
+          </p>
+          <button
+            className={`absolute right-[-14px] top-[-14px] w-[30px] h-[30px] flex justify-center items-center bg-light-bg-300 dark:bg-dark-bg-300 border-none rounded-full p-1 cursor-pointer transition-background ease-in-out duration-300 hover:bg-light-primary-100 hover:dark:bg-dark-primary-100`}
+            onClick={() => setIsDeleting(true)}
+          >
+            <DeleteIcon />
+          </button>
+        </li>
+      </div>
     </div>
   );
 }
--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable react/prop-types */
 
 function TodoList({ children }) {
-  return (
-    <ul className="py-14 pr-7 pl-4 w-full flex flex-col space-y-4">
-      {children}
-    </ul>
-  );
+  return <ul className="py-14 pr-7 pl-4 w-full flex flex-col">{children}</ul>;
 }
 
 export { TodoList };
